49a77ed98c49445582f73ada73ab448f9314e6b1
Leon <leonwuya@gmail.com>
2026-03-11 20:06:21 -0700

    inital project setup

---
+++ src/main/java/com/leon/marketing_analytics/MarketingAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.leon.marketing_analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketingAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketingAnalyticsApplication.class, args);
+	}
+
+}
